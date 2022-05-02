b99d5659e8895a2a2f5121d8353ae4be3b409a9d
Giuseppe Falcidia <giuseppefalcidia@Giuseppes-MacBook-Pro.local>
2022-05-02 23:50:11 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import QRCode from 'qrcode'
+import { useState } from 'react'
+
+function App() {
+  const [url, setUrl] = useState('');
+  const [qrcode, setQrcode] = useState('');
+
+  const GenerateQRCode = () => {
+    QRCode.toDataURL(url, 
+      // options qrcode resolution/visualization
+       { 
+         width: 800,
+         margin: 2,
+         color: {
+           dark: '#ffffffff',
+           light: '#000000ff'
+         }
+       },
+      (err, url) => {
+      if (err) return console.error(err)
+      setQrcode(url)
+    })
+  }
+
+  return (
+    <div className="app">
+     <h1>QR Code Generator</h1>
+     <input 
+     type="text"
+     placeholder="e.g. https://google.com" 
+     value={url}
+     onChange={(evt) => setUrl(evt.target.value)} />
+     <button onClick={GenerateQRCode}>Generate</button>
+
+      {qrcode && <>
+        <img src={qrcode} />
+        <a href={qrcode} download="qrcode.png">Download</a>
+      </>}
+    </div>
+  )
+}
+
+export default App;
